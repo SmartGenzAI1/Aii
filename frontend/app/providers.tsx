f96747a5d6311abfe248f949add33438098c744d
Saff9 <saffanakbar942@gmail.com>
2025-12-26 18:22:49 +0530

    Update providers.tsx

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -6,11 +6,10 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
 
-export function Providers({ children }: { children: ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
+    <SessionProvider refetchOnWindowFocus={false}>
       {children}
     </SessionProvider>
   );
